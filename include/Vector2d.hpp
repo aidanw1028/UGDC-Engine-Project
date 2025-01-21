1281dad9d1aabe231ea2f83cdf41e2745dc2e99a
Aidan Waring <aidanw1028@gmail.com>
2025-01-21 11:12:31 -0600

    Added vector2d header file with class definition:

---
+++ include/Vector2d.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <iostream>
+
+class Vector2d {
+    private:
+        double x;
+        double y;
+
+    public:
+     Vector2d()
+        : x{0.0}, y{0.0}
+    {} // default constructor for no input
+
+    // overloaded constructor for when user input is defined
+
+    Vector2d(double xIn, double yIn)
+        : x{xIn}, y{yIn}
+    {}
+
+    // basic setters with inlines
+    inline void SetX(double xIn) {this->x = xIn;}
+    inline void SetY(double yIn) {this->y = yIn;}
+
+    //vector operators
+
+    Vector2d operator+(const Vector2d& vector) {
+        Vector2d result;
+        result.x = x + vector.x;
+        result.y = y + vector.y;
+        return result;
+    }
+
+    Vector2d operator-(const Vector2d& vector) {
+        Vector2d result;
+        result.x = x - vector.x;
+        result.y = y - vector.y;
+        return result;
+    }
+
+    // minus 1d
+
+    Vector2d operator-() {
+        Vector2d result;
+        result.x = -x;
+        result.y = -y;
+        return result;
+    }
+
+    friend std::ostream& operator<< (std::ostream& os, const Vector2d& v);
+
+};
+inline std::ostream operator<< (std::ostream& os, const Vector2d& vectorIn) {
+    os << "(" << v.x << ", " << v.y << ")";
+    return os;
+}
