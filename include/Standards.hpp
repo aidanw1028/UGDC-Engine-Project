cc5d78d0843b3ca5693250c2472e69eb8d24110d
Aidan Waring <aidanw1028@gmail.com>
2025-01-21 14:45:54 -0600

    Added Cstdtypes to Standards.hpp

--- include/Standards.hpp
+++ include/Standards.hpp
@@ -1,1 +1,14 @@
 #pragma once
+#include <cstdint>
+#include <string>
+typedef std::string string;
+typedef std::size_t size;
+typedef int64_t Int64;
+typedef int32_t Int32;
+typedef int16_t Int16;
+typedef int8_t Int8;
+
+typedef uint64_t UInt64;
+typedef uint32_t UInt32;
+typedef uint16_t UInt16;
+typedef uint8_t UInt8;
